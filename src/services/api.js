acf24807d3fd86b2747e64eb57da9bcbdff501fd
”John <102276922+UlianaNad@users.noreply.github.com>
2023-10-31 16:18:25 +0200

    di

--- src/services/api.js
+++ src/services/api.js
@@ -2,19 +2,16 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
 export const fetchPhotos = async params => {
-  const PARAMS = new URLSearchParams({
-    key: '39829735-722e175b01b967c16f6acbd5c',
-    // q: this.query,
-    page: 1,
-    per_page: 12,
-    // safesearch: true,
-    // orientation: 'horizontal',
-    // image_type: 'photo',
-    // lang: 'en',
-    // lang: 'fi',
-  });
   try {
-    const { data } = await axios.get(`?${PARAMS}`);
+    const { data } = await axios.get('', {
+      params: {
+        key: '39829735-722e175b01b967c16f6acbd5c',
+        // q: this.query,
+        page: 1,
+        per_page: 12,
+        ...params,
+      },
+    });
     return data;
   } catch (error) {
     return error.message;
